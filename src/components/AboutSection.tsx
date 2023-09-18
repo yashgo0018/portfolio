906b9b2054a688a1f47c329fde46523691be373c
Yash Goyal <yashgo0018@gmail.com>
2023-09-19 00:37:25 +0530

    fixed sorting in awards section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -8,8 +8,8 @@ export default function AboutSection({
   return (
     <div className="h-screen relative md:mx-0 mx-10" id="about">
       <div className="absolute top-[50%] translate-y-[-50%] left-0 right-0">
-        <div className="lg:grid lg:grid-cols-3 gap-6">
-          <div className="flex lg:col-span-1 pb-10 mb-3 lg:mb-0">
+        <div className="lg:grid lg:grid-cols-3 gap-6 pb-10">
+          <div className="flex lg:col-span-1 lg:mb-0 mb-10 lg:mb-0">
             <div className="flex-1"></div>
             <img
               src={image?.asset.url}
@@ -18,8 +18,15 @@ export default function AboutSection({
             />
             <div className="flex-1"></div>
           </div>
-          <div className="col-span-2 text-xl md:text-2xl">
-            {longDescription}
+          <div className="col-span-2 lg:relative text-xl xl:text-2xl text-justify">
+            <div className="lg:absolute lg:top-[50%] lg:translate-y-[-50%]">
+              {longDescription?.split("\n").map((line) => (
+                <div>
+                  {line}
+                  <br />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
